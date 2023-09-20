4aff19728f5307539e2d0c72b06c99d3192bc157
Kristijan <97643470+krissgoshevski@users.noreply.github.com>
2023-09-20 20:26:10 +0200

    Update show.blade.php

--- resources/views/company/show.blade.php
+++ resources/views/company/show.blade.php
@@ -1,19 +1,22 @@
-@extends('layouts.app') <!-- Заменете 'layouts.app' со вашата лажици, доколку користите различна лажица -->
+@extends('layouts.app')
+@extends('layouts.errors')
+@extends('layouts.navbar')
+
+@section('title', 'Show page')
 
 @section('content')
     <div class="container">
-        <h1>Детали за компанијата</h1>
+        <h1>Details of {{ $company->name }} company</h1>
 
         <ul>
-            <li><strong>Име на компанија:</strong> {{ $company->name }}</li>
-            <li><strong>Е-пошта:</strong> {{ $company->email }}</li>
-            <li><strong>Лого:</strong> <img src="{{ asset('storage/' . $company->logo) }}" alt="Лого на компанијата"></li>
-            <li><strong>Веб-сајт:</strong> <a href="{{ $company->website }}">{{ $company->website }}</a></li>
+            <li><strong>Name:</strong> {{ $company->name }}</li>
+            <li><strong>Email:</strong> {{ $company->email }}</li>
+            <li><strong>Logo:</strong> <img src="{{ $company->logo }}" alt="Company Logo" height="200px" width="200px"></li>
+            <li><strong>Website:</strong> <a href="{{ $company->website }}">{{ $company->website }}</a></li>
             <li><strong>Country:</strong> <a href="{{ $company->country->name }}">{{ $company->country->name }}</a></li>
 
-            <!-- Додадете ги другите детали што сакате да ги прикажете -->
         </ul>
         
-        <a href="{{ route('company.index') }}">Назад кон листата со компании</a>
+        <a href="{{ route('company.index') }}">Back to view all companies</a>
     </div>
 @endsection
